3b05984de43421ad354d3281f587477941651af9
Ankita Luthra <ankitaluthra1@gmail.com>
2021-04-21 12:55:49 +0530

    adds solution for common prefix in string array

---
+++ src/main/java/leetcode/datastructures/LongestCommonPrefix.java
@@ -0,0 +1,35 @@
+package leetcode.datastructures;
+
+public class LongestCommonPrefix {
+
+    public String longestCommonPrefix(String[] strs) {
+        if(strs.length == 0)
+            return "";
+        int index = 0;
+        StringBuilder sg = new StringBuilder();
+        while (true) {
+            Character c = null;
+            for (String s : strs) {
+                if (index >= s.length())
+                    return sg.toString();
+                if (c == null)
+                    c = s.charAt(index);
+                else {
+                    if (s.charAt(index) != c)
+                        return sg.toString();
+                }
+            }
+            sg.append(c);
+            index++;
+        }
+    }
+
+    public static void main(String[] args) {
+        LongestCommonPrefix lp = new LongestCommonPrefix();
+        String[] strs = {""};
+        String prefix = lp.longestCommonPrefix(strs);
+
+        System.out.println(prefix);
+    }
+
+}
