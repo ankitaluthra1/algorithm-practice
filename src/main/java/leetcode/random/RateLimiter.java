3ce19f0f94fed0da25b484e0708e4c73650c5536
Ankita Luthra <ankita.luthra@walmart.com>
2022-10-17 14:29:49 +0530

    refactors rate limiter to Rate limit for X requests per Y mins per client

--- src/main/java/leetcode/random/RateLimiter.java
+++ src/main/java/leetcode/random/RateLimiter.java
@@ -1,82 +1,54 @@
 package leetcode.random;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.*;
 
-// Rate limit for 3 requests per 5 mins
+// Rate limit for X requests per Y mins per client
 public class RateLimiter {
 
-    int capacity;
+    class Threshold {
+        private int threshold;
+        private int seconds;
 
-    public RateLimiter(int capacity) {
-        this.capacity = capacity;
+        public Threshold(int threshold, int seconds) {
+            this.threshold = threshold;
+            this.seconds = seconds;
+        }
+
+
+    }
+    public RateLimiter() {
+        requestQueueMap = new HashMap<>();
+        customerThresholdMap = new HashMap<>();
     }
 
-    Map<String, List<Request>> requestMap;
+    Map<String, Queue<String>> requestQueueMap;
+    Map<String, Threshold> customerThresholdMap;
 
-    private void rateLimit(String user, Request request) {
-        if (!requestMap.containsKey(user) || requestMap.get(user).size() < capacity) {
-            List<Request> list = requestMap.getOrDefault(user, new ArrayList<>());
-            list.add(request);
-            requestMap.put(user, list);
-            System.out.println("Not throttled");
-            return;
+    private boolean rateLimit(String customerId) {
+        Instant currentTime = Instant.now(); //captures the current time in UTC
+        removeOlderRequestFromRequestQueueMap(customerId, currentTime);
+        Queue<String> existingRequests = requestQueueMap.get(customerId);
+        if (existingRequests.size() < customerThresholdMap.get(customerId).threshold) {
+            existingRequests.add(currentTime.toString());
+            requestQueueMap.put(customerId, existingRequests);
+            return true;
         }
-        boolean throttled = true;
-        List<Request> requests = requestMap.get(user);
-
-        for (Request r : requests) {
-            if (request.timestamp - r.timestamp >= 5) {
-                requests.remove(r);
-                throttled = false;
-                continue;
-            }
-            break;
-        }
-        if (throttled) {
-            System.out.println("Request Throttled");
-            return;
-        }
-        requests.add(request);
-        requestMap.put(user, requests);
-        System.out.println("Not throttled");
+        return false;
     }
 
-    public static void main(String[] args) {
-        RateLimiter rateLimiter = new RateLimiter(3);
-        Request request1 = new Request(1, "/test");
-        Request request2 = new Request(2, "/test");
-        Request request3 = new Request(2, "/test");
-        Request request4 = new Request(3, "/test");
-        Request request5 = new Request(3, "/test");
-        Request request6 = new Request(3, "/test");
-        Request request7 = new Request(4, "/test");
-        Request request8 = new Request(6, "/test");
-        Request request9 = new Request(6, "/test");
-        Request request10 = new Request(6, "/test");
-        rateLimiter.rateLimit("user1", request1);
-        rateLimiter.rateLimit("user1", request2);
-        rateLimiter.rateLimit("user1", request3);
-        rateLimiter.rateLimit("user1", request4);
-        rateLimiter.rateLimit("user1", request5);
-        rateLimiter.rateLimit("user1", request6);
-        rateLimiter.rateLimit("user1", request7);
-        rateLimiter.rateLimit("user1", request8);
-        rateLimiter.rateLimit("user1", request9);
-        rateLimiter.rateLimit("user1", request10);
-
-
+    private void removeOlderRequestFromRequestQueueMap(String customerId, Instant currentTime) {
+        Queue<String> existingRequestQueue = requestQueueMap.get(customerId);
+        while (existingRequestQueue.size() > 0) {
+            Instant lastRequestInstant = Instant.parse(existingRequestQueue.peek());
+            if (Duration.between(currentTime, lastRequestInstant).getSeconds() > customerThresholdMap.get(customerId).seconds)
+                existingRequestQueue.poll();
+            else
+                break;
+        }
     }
 
 }
 
-class Request {
-    long timestamp;
-    String url;
-
-    public Request(long timestamp, String url) {
-        this.timestamp = timestamp;
-        this.url = url;
-    }
-}
