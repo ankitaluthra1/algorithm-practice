22a9df91842cddcf3b00d464670d2476d867ebe2
Ankita Luthra <ankita.luthra@walmart.com>
2022-10-18 12:06:22 +0530

    adds credits logic in rate limiter

--- src/main/java/leetcode/random/RateLimiter.java
+++ src/main/java/leetcode/random/RateLimiter.java
@@ -2,22 +2,44 @@ package leetcode.random;
 
 import java.time.Duration;
 import java.time.Instant;
-import java.time.LocalDateTime;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Queue;
 
 // Rate limit for X requests per Y mins per client
 public class RateLimiter {
 
     class Threshold {
+        private final int MAX_CAPACITY = 500;
         private int threshold;
         private int seconds;
+        private int credits;
 
         public Threshold(int threshold, int seconds) {
             this.threshold = threshold;
             this.seconds = seconds;
         }
 
+        public void addCredits(int additionalCredits){
+            int count = Math.min(MAX_CAPACITY - this.credits, additionalCredits);
+            this.credits = this.credits + count;
+        }
 
+        public int thresholdWithCredits(){
+            return this.credits + this.threshold;
+        }
+
+        public int getThreshold() {
+            return threshold;
+        }
+
+        public int getSeconds() {
+            return seconds;
+        }
+
+        public void deductCredit() {
+            this.credits--;
+        }
     }
     public RateLimiter() {
         requestQueueMap = new HashMap<>();
@@ -31,9 +53,15 @@ public class RateLimiter {
         Instant currentTime = Instant.now(); //captures the current time in UTC
         removeOlderRequestFromRequestQueueMap(customerId, currentTime);
         Queue<String> existingRequests = requestQueueMap.get(customerId);
-        if (existingRequests.size() < customerThresholdMap.get(customerId).threshold) {
+        Threshold threshold = customerThresholdMap.get(customerId);
+        if (existingRequests.size() < threshold.thresholdWithCredits()) {
+            if (existingRequests.size() >= threshold.getThreshold()){
+                threshold.deductCredit();
+            }
             existingRequests.add(currentTime.toString());
             requestQueueMap.put(customerId, existingRequests);
+            threshold.addCredits(customerThresholdMap.get(customerId).threshold - existingRequests.size());
+            customerThresholdMap.put(customerId, threshold);
             return true;
         }
         return false;
@@ -43,7 +71,7 @@ public class RateLimiter {
         Queue<String> existingRequestQueue = requestQueueMap.get(customerId);
         while (existingRequestQueue.size() > 0) {
             Instant lastRequestInstant = Instant.parse(existingRequestQueue.peek());
-            if (Duration.between(currentTime, lastRequestInstant).getSeconds() > customerThresholdMap.get(customerId).seconds)
+            if (Duration.between(currentTime, lastRequestInstant).getSeconds() > customerThresholdMap.get(customerId).getSeconds())
                 existingRequestQueue.poll();
             else
                 break;
